a735d6a4abca53037d942c3ad1721135374500e1
MICHEAL CURLEY <G00340243@gmit.ie>
2018-03-01 23:05:25 +0000

    shunting yard tutorial done

--- rega/shunt.go
+++ rega/shunt.go
@@ -4,15 +4,42 @@ import (
 	"fmt"
 )
 
+
 func intoPostFix(infix string) string {
-	specials := map[rune]int{"*":10, ".":9, "|":8}
-
+	//precedence map
+	specials := map[rune]int{'*':10, '.':9, '|':8}
+	//initialsing stacks
 	postFix, stack := []rune{}, []rune{}
-
+	//range loop converts string to char array (UTF-8)
 	for _, r := range infix{
-
+		switch{
+		case r == '(':
+			stack = append(stack, r)
+		case r == ')':
+			for stack[len(stack)-1] != '('{
+				//next 2 lines pop character off stack and append it to returned postfix
+				postFix = append(postFix, stack[len(stack)-1])
+				stack = stack[:len(stack)-1]
+			}
+			//pops off next character after brackets
+			stack = stack[:len(stack)-1]
+		case specials[r] > 0:
+			//if stack isn't empty and the precedence off current char is less/equals to the precedence of whats on the stack
+			for len(stack) > 0 && specials[r] <= specials[stack[len(stack)-1]]{
+				//next 2 lines pop character off stack and append it to returned postfix
+				postFix = append(postFix, stack[len(stack)-1])
+				stack = stack[:len(stack)-1]
+			}
+			stack = append(stack, r)
+		default: 
+			postFix = append(postFix, r)
+		}
 	}
-
+	for len(stack)>0{
+		//next 2 lines pop character off stack and append it to returned postfix
+		postFix = append(postFix, stack[len(stack)-1])
+		stack = stack[:len(stack)-1]
+	}
 	return string(postFix)
 }
 
@@ -25,11 +52,11 @@ func main() {
 	fmt.Println("Infix:    ", "(a.(b|d))*")
 	fmt.Println("Postfix:  ", intoPostFix("(a.(b|d))*"))
 
-	//Answer: ab.c*.
+	//Answer: abd|.c*.
 	fmt.Println("Infix:    ", "a.(b|d).c*")
 	fmt.Println("Postfix:  ", intoPostFix("a.(b|d).c*"))
 
-	//Answer: ab.c*.
+	//Answer: abb|+.c.
 	fmt.Println("Infix:    ", "a.(b|b)+.c")
 	fmt.Println("Postfix:  ", intoPostFix("a.(b|b)+.c"))
 }
